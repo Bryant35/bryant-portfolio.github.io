aaea8e3773a7c6423aa31e74cbd8781089e80fd4
Bat <banthony@student.ciputra.ac.id>
2024-07-10 22:26:40 +0700

    Update to New View

---
+++ src/tailwind.config.js
@@ -0,0 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{html,js}",
+//     "./node_modules/flowbite/**/*.js"
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         clifford: '#da373d',
+//       }
+//     },
+//   },
+//   // plugins: [
+//   //   require('flowbite/plugin') 
+//   // ],
+// }
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'orange-text': '#FF7B00',
+        'orange-fruit': '#FFA652',
+        'orange-theme': '#FFF4DF',
+        'orange-light': '#FFB76B',
+        'orange-bright': '#FFCD90',
+      }
+    },
+  },
+};
